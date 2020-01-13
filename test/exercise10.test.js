34fa5c7940e18e962ed713fd08107dcbcbf7c811
Lewizard98 <lewizkeech01@gmail.com>
2020-01-13 14:54:13 +0000

    completed Exercises

--- test/exercise10.test.js
+++ test/exercise10.test.js
@@ -5,8 +5,6 @@ function weirdNumbers(delta) {
   return (1 + delta - Math.random());
 }
 
-/** Delete this line and the one at the bottom when you're ready for exercise 7
- 
 
     suite('Unit Tests Exercise 10 - Approximation',function(){
       // 10 - .approximately 
@@ -15,14 +13,12 @@ function weirdNumbers(delta) {
       // Choose the minimum range (3rd parameter) to make the test always pass
       // it should be less than 1
       test('Approximation part 1', function(){
-        assert.approximately(weirdNumbers(0.5) , 1, //edit this
+        assert.approximately(weirdNumbers(0.5) , 1, 0.5,
          0 );
       });
       test('Approximation part 2', function(){
-        assert.approximately(weirdNumbers(0.2) , 1, //edit this
+        assert.approximately(weirdNumbers(0.2) , 1, 1,
          0 );
       });
     })
 
-  
-// Delete this line when you're ready for exercise 10 **/
